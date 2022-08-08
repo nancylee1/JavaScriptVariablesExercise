a7fc257a81524bac0d2c9b82f793eec62c763858
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-08-08 15:17:22 -0400

    made pretty

--- app.js
+++ app.js
@@ -3,25 +3,30 @@
 const firstName = "Nancy";
 let lastName = "Lee";
 var age = 40;
+let exercise1 = document.getElementById("exercise1");
+exercise1.innerHTML = `${firstName} ${lastName} ${age}`;
 
 //Exercise 2: Assigning Values to Variables
 
-let fullName = firstName + " " + lastName;
-console.log(fullName);
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
 
 //Bonus
-let fullName = '${firstName} ${lastName}';
-
-//Exercise 3: Declaring and Assigning Values to Variables
+let fullName = `${firstName} ${lastName}`;
 let city = "New York";
 let passTime = "singing";
-let myStory = 'Hello! My name is ${fullName} and I am from ${city}. I enjoy ${passTime}, biking, and coding!'
-console.log(myStory);
+let myStory = `Hello! My name is ${fullName} and I am from ${city}. I enjoy ${passTime}, biking, and coding!`;
+let exercise2 = document.getElementById("exercise2");
+exercise2.innerHTML = myStory;
 
+
+//Exercise 3: Declaring and Assigning Values to Variables
 let language = "JavaScript";
 let createdYear = 1995;
 let isCaseSensitive = true;
-console.log('${language} was created in ${createdYear}.')
+let exercise3 = document.getElementById("exercise3");
+exercise3.innerHTML = `${language} was created in ${createdYear}.`;
+// console.log('${language} was created in ${createdYear}.')
 
 //Exercise 4: Declaring and Assigning Values to Variables x2
 
@@ -32,7 +37,8 @@ let salePercentage = 15;
 let stock = 0;
 let inStock = false;
 let selectedSize = "M";
-console.log('The ${item} is on sale for ${price} at ${salePercentage}% off in the size ${selectedSize} and stock level is ${stock}.')
+let exercise4 = document.getElementById("exercise4")
+exercise4.innerHTML = `The ${item} is on sale for ${price} at ${salePercentage}% off in the size ${selectedSize} and stock level is ${stock}.`;
 
 //Exercise 5: Declaring and Assigning Values to Variables x3
 
@@ -41,4 +47,5 @@ let author = "Patrick Rothfuss";
 let paceCount = 722;
 let bookmark = 456;
 let hasRead = true; 
-console.log('The book ${title} by ${author} has a pace count of ${paceCount} and bookmarked at page ${bookmark}.')
+let exercise5 = document.getElementById("exercise5")
+exercise5.innerHTML =  `The book ${title} by ${author} has a pace count of ${paceCount} and bookmarked at page ${bookmark}.`;
